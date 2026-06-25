e30d20e0d341c9b08d0093412400c428e5adcd2b
pietromurillo <pietromurillo19@gmail.com>
2026-06-25 20:24:25 -0300

    initial: setup spring boot project with web, jpa, and h2 dependencies

---
+++ src/test/java/com/github/pietromurillo/customer_support/CustomerSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pietromurillo.customer_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
